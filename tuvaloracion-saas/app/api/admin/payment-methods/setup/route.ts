7ad520160b55ba2c3bbecb1646549365634eea7d
alu0100536690 <merquis85@gmail.com>
2025-08-10 17:09:32 +0200

    Corrigiendo suscripcion al finalizar

--- tuvaloracion-saas/app/api/admin/payment-methods/setup/route.ts
+++ tuvaloracion-saas/app/api/admin/payment-methods/setup/route.ts
@@ -10,7 +10,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 
 export async function POST(request: Request) {
   try {
-    const headersList = headers();
+    const headersList = await headers();
     const authHeader = headersList.get('authorization');
 
     const user = verifyAuth(authHeader || ''); // Pasamos el header de autorización
